9882687ad85fd572ea7902b300c041beee15185b
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-07-23 22:44:12 -0300

    update

--- js/modules/faq.js
+++ js/modules/faq.js
@@ -1,14 +1,16 @@
 export default function initFaq(){
   const perguntas = document.querySelectorAll("[data-faq]");
 
-  perguntas[0].classList.add("active");
-  perguntas[0].nextElementSibling.classList.add("active");
-  function handleClick(event){
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("active");
+  if(perguntas.length){
+    perguntas[0].classList.add("active");
+    perguntas[0].nextElementSibling.classList.add("active");
+    function handleClick(event){
+      this.classList.toggle("active");
+      this.nextElementSibling.classList.toggle("active");
+    }
+  
+    perguntas.forEach((pergunta) =>{
+      pergunta.addEventListener("click",handleClick);
+    })
   }
-
-  perguntas.forEach((pergunta) =>{
-    pergunta.addEventListener("click",handleClick);
-  })
 }
