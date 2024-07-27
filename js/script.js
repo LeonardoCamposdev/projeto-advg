1250410a738e475170302aa6515f0eee041ebc4c
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-07-27 18:55:46 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFaq from "./modules/faq.js";
+import initScrollSuave from "./modules/scroll-suave.js";
 
 initDropdownMenu();
 initMenuMobile();
 initFaq();
+initScrollSuave();
