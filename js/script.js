46addf86bc809ed2fb3dea33eba455dda516fd63
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-07-27 20:37:53 -0300

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFaq from "./modules/faq.js";
 import initScrollSuave from "./modules/scroll-suave.js";
+import initPluginScroll from "./modules/plugin.-scroll.js";
 import { initAnimacaoScrollLeft, initAnimacaoScrollRight, initAnimacaoScrollUp, initAnimacaoScrollDown } from "./modules/scroll-animacao.js";
 
 
@@ -9,6 +10,7 @@ initDropdownMenu();
 initMenuMobile();
 initFaq();
 initScrollSuave();
+initPluginScroll();
 initAnimacaoScrollLeft();
 initAnimacaoScrollRight();
 initAnimacaoScrollUp();
