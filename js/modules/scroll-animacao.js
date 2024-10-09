78662502c815cabae2af522b64526221ccd1a054
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-10-09 19:07:50 -0300

    update

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,67 +1,67 @@
-export  function initAnimacaoScrollLeft(){
-    const sections = document.querySelectorAll('[data-anime="scroll-left"]');
-    const windowMetade = window.innerHeight * 0.6;
-    
-    function animaScroll(){
-      sections.forEach((section) =>{
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-    
-        if(sectionTop < 0){
-          section.classList.add("ativo");
-        }
-      })
-    }
-    window.addEventListener("scroll", animaScroll);
+export function initAnimacaoScrollLeft() {
+  const sections = document.querySelectorAll('[data-anime="scroll-left"]');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+
+      if (sectionTop < 0) {
+        section.classList.add("ativo");
+      }
+    });
   }
 
-  export  function initAnimacaoScrollRight(){
-    const sections = document.querySelectorAll('[data-anime="scroll-right"]');
-    const windowMetade = window.innerHeight * 0.6;
-    
-    function animaScroll(){
-      sections.forEach((section) =>{
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-    
-        if(sectionTop < 0){
-          section.classList.add("ativo");
-        }
-      })
-    }
-    
-    window.addEventListener("scroll", animaScroll);
+  window.addEventListener("scroll", animaScroll);
+}
+
+export function initAnimacaoScrollRight() {
+  const sections = document.querySelectorAll('[data-anime="scroll-right"]');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+
+      if (sectionTop < 0) {
+        section.classList.add("ativo");
+      }
+    });
   }
 
-  export  function initAnimacaoScrollUp(){
-    const sections = document.querySelectorAll('[data-anime="scroll-up"]');
-    const windowMetade = window.innerHeight * 0.8;
-    
-    function animaScroll(){
-      sections.forEach((section) =>{
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-    
-        if(sectionTop < 0){
-          section.classList.add("ativo");
-        }
-      })
-    }
-    
-    window.addEventListener("scroll", animaScroll);
+  window.addEventListener("scroll", animaScroll);
+}
+
+export function initAnimacaoScrollUp() {
+  const sections = document.querySelectorAll('[data-anime="scroll-up"]');
+  const windowMetade = window.innerHeight * 0.8;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+
+      if (sectionTop < 0) {
+        section.classList.add("ativo");
+      }
+    });
   }
 
-  export  function initAnimacaoScrollDown(){
-    const sections = document.querySelectorAll('[data-anime="scroll-down"]');
-    const windowMetade = window.innerHeight * 0.6;
-    
-    function animaScroll(){
-      sections.forEach((section) =>{
-        const sectionTop = section.getBoundingClientRect().top - windowMetade;
-    
-        if(sectionTop < 0){
-          section.classList.add("ativo");
-        }
-      })
-    }
-    
-    window.addEventListener("scroll", animaScroll);
+  window.addEventListener("scroll", animaScroll);
+}
+
+export function initAnimacaoScrollDown() {
+  const sections = document.querySelectorAll('[data-anime="scroll-down"]');
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+
+      if (sectionTop < 0) {
+        section.classList.add("ativo");
+      }
+    });
   }
 
+  window.addEventListener("scroll", animaScroll);
+}
